2dd561012d50bd4817373035f75be2b0a82458bc
Elsayed <Mostafa.Elsayed@td.com>
2019-05-30 14:30:08 -0400

    initial commit for schematics module

---
+++ schematics/src/feature/schema.ts
@@ -0,0 +1,10 @@
+export interface Schema {
+    // The name of the feature.
+    name: string;
+
+    // The path to create the feature.
+    path?: string;
+
+    // The name of the project.
+    project?: string;
+}
